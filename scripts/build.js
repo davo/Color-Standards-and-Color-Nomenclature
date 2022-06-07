b23485c86441367c1de9bce73188265e8adb8a78
David Aerne <meodai@gmail.com>
2022-06-07 18:16:00 +0200

    chore(build): adds a script to translate the names to HWB

--- scripts/build.js
+++ scripts/build.js
@@ -1,10 +1,54 @@
 const fs = require('fs');
 
 const {
-  parseCSVString
+  parseCSVString,
+  convertHwbToRgb,
+  rgbToHex
 } = require ('./lib.js');
 
 const RAWcsv = fs.readFileSync('./src/colornames.csv', 'utf8');
 const list = parseCSVString(RAWcsv);
 
+const wbMods = {
+  'g': [70, 0],
+  'f': [45, 0],
+  'e': [32, 0],
+  'd': [22.5, 0],
+  'c': [15, 0],
+  'b': [9.5,0],
+  'a': [5, 0],
+  '-': [0, 0],
+  'h': [0, 26],
+  'i': [0, 45],
+  'j': [0, 49],
+  'k': [0, 70.5],
+  'l': [0, 80],
+  'm': [0, 87.5],
+  'n': [0, 94],
+};
+
+function wbModStringtoWB(str){
+  if (wbMods.hasOwnProperty(str)){
+    return wbMods[str];
+  } else {
+    return [0,0];
+  }
+}
+
+const hexes = [];
+list.entries.forEach(entry => {
+  // https://www.gutenberg.org/files/63087/63087-h/63087-h.htm#tones
+  const hueBase100 = parseInt(/\d+/g.exec(entry["Color or Hue Number"])[0]);
+  const hueBase360 = hueBase100 * 3.6;
+
+  const hwb = [hueBase360, ...wbModStringtoWB(entry.Tone)];
+  const calculatedRGB = convertHwbToRgb(hwb);
+  const calculatedHEX = rgbToHex(calculatedRGB);
+
+  hexes.push(calculatedHEX);
+});
+
+//fs.writeFileSync('./dist/HWBhexes.json', JSON.stringify(hexes, null, 2));
+
+// export the list as a JSON file
 fs.writeFileSync('./dist/colornames.json', JSON.stringify(list.entries, null, 2));
--- scripts/lib.js
+++ scripts/lib.js
@@ -52,6 +52,91 @@ function parseCSVString (csvString, csvDelimitor = ',', csvNewLine = '\r\n') {
   };
 }
 
+// https://jsfiddle.net/seamusleahy/12eL2qse/
+function convertHwbToRgb(hwb) {
+  var h = hwb[0] / 360;
+  var wh = hwb[1] / 100;
+  var bl = hwb[2] / 100;
+  var ratio = wh + bl;
+  var i;
+  var v;
+  var f;
+  var n;
+
+  // wh + bl cant be > 1
+  if (ratio > 1) {
+    wh /= ratio;
+    bl /= ratio;
+  }
+
+  i = Math.floor(6 * h);
+  v = 1 - bl;
+  f = 6 * h - i;
+
+  if ((i & 0x01) !== 0) {
+    f = 1 - f;
+  }
+
+  n = wh + f * (v - wh); // linear interpolation
+
+  var r;
+  var g;
+  var b;
+  switch (i) {
+    default:
+    case 6:
+    case 0:
+      r = v;
+      g = n;
+      b = wh;
+      break;
+    case 1:
+      r = n;
+      g = v;
+      b = wh;
+      break;
+    case 2:
+      r = wh;
+      g = v;
+      b = n;
+      break;
+    case 3:
+      r = wh;
+      g = n;
+      b = v;
+      break;
+    case 4:
+      r = n;
+      g = wh;
+      b = v;
+      break;
+    case 5:
+      r = v;
+      g = wh;
+      b = n;
+      break;
+  }
+
+  return [
+    Math.round(r * 255),
+    Math.round(g * 255),
+    Math.round(b * 255)
+  ];
+}
+
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(rgbArr) {
+  const [r, g, b] = rgbArr;
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+
 module.exports = {
-  parseCSVString
+  parseCSVString,
+  convertHwbToRgb,
+  rgbToHex
 };
